6a7cd4895f26d003a5791bcbeb319879def44e6d
rc <932194632@qq.com>
2015-11-18 21:23:09 +0800

    add log 3

--- mygithubtest/mygithubtest/main.m
+++ mygithubtest/mygithubtest/main.m
@@ -13,6 +13,7 @@ int main(int argc, const char * argv[]) {
         // insert code here...
         NSLog(@"Hello, World!");
          NSLog(@"Hello, World!");
+         NSLog(@"Hello, World!");
     }
     return 0;
 }
