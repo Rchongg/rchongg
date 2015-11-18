674eea3bcbf84f28eb2d6d181d325299aefbd998
rc <932194632@qq.com>
2015-11-18 21:13:43 +0800

    初始化下面

---
+++ mygithubtest/mygithubtest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  mygithubtest
+//
+//  Created by rc on 15/11/18.
+//  Copyright © 2015年 rc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
